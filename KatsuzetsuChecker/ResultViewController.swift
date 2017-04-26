934acf761d9f2250014e210d9a32b6db0a366f02
gachaa <mae.gachahero.104@gmail.com>
2017-04-26 17:02:05 +0900

    CheckViewController、ResultViewController、ListTableViewController、AddViewControllerを追加した。

---
+++ KatsuzetsuChecker/ResultViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ResultViewController.swift
+//  KatsuzetsuChecker
+//
+//  Created by 前本英里香 on 2017/04/26.
+//  Copyright © 2017年 None. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
